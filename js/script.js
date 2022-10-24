629630a0ea8272f1abaef0a6ea3978cde260b485
Bruno Santana <brunosantanadev@gmail.com>
2022-10-24 00:23:39 -0300

    Ajustes finais do responsivo e espaçamentos

--- js/script.js
+++ js/script.js
@@ -3,7 +3,9 @@ document.documentElement.className += " js";
 import initScrollSuaveLinkInterno from "./modulos/scrollSuaveLinksInterno.js";
 import initAnimaNome from "./modulos/animaNome.js";
 import initMenuHamburgue from "./modulos/menu-hamburgue.js";
+import initBarrinhas from "./modulos/barrinhas.js";
 
 initScrollSuaveLinkInterno();
 initAnimaNome();
 initMenuHamburgue();
+initBarrinhas();
