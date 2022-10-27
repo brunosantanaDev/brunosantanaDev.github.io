cf03ce8b8e0a3ed477bf46c3418cedb1ca109fe0
Bruno Santana <brunosantanadev@gmail.com>
2022-10-27 18:31:12 -0300

    adicionei animação ao bookmark

--- js/modulos/anima-book.js
+++ js/modulos/anima-book.js
@@ -1,5 +1,17 @@
 export default function initAnimaBook() {
+  const section = document.querySelector(".js [data-js-book]")
+  const bookmark = document.querySelector(".bookmark");
 
+  if(section){
+    function handleBookMark(){
+      if(section.getBoundingClientRect().top < -300){
+        bookmark.classList.add("ativo")
+      }else{
+        bookmark.classList.remove("ativo")
+      }
+    } 
+    window.addEventListener("scroll", handleBookMark);
+  }
 }
 
 
