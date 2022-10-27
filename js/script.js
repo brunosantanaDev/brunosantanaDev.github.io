2d62924734992b4574286be07876eda5aad80c72
Bruno Santana <brunosantanadev@gmail.com>
2022-10-27 18:30:20 -0300

    alterei a cor do paragrafo sessao sobre

--- js/script.js
+++ js/script.js
@@ -4,10 +4,12 @@ import initScrollSuaveLinkInterno from "./modulos/scrollSuaveLinksInterno.js";
 import initAnimaNome from "./modulos/animaNome.js";
 import initMenuHamburgue from "./modulos/menu-hamburgue.js";
 import initBarrinhas from "./modulos/barrinhas.js";
-import animacaoScroll from "./modulos/animacaoScroll.js";
+import initAnimacaoScroll from "./modulos/animacaoScroll.js";
+import initAnimaBook from "./modulos/anima-book.js";
 
 initScrollSuaveLinkInterno();
 initAnimaNome();
 initMenuHamburgue();
 initBarrinhas();
-animacaoScroll();
+initAnimacaoScroll();
+initAnimaBook();
